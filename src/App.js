8c8a844b0a204595383d484764de7a54523eabb2
Celia Saumell <56240891+celiasaumell@users.noreply.github.com>
2021-03-24 23:49:58 -0500

    dynamic content

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ class App extends Component {
         <h1>Hi!! I'm a React App</h1>
         <p>This is really working</p>
         <Person />
+        <Person />
+        <Person />
+        
       </div>
 
     );
