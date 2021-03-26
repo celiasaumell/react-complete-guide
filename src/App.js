c6e6451ba1f61d1733e85f174c499fc3edab8cad
Celia Saumell <56240891+celiasaumell@users.noreply.github.com>
2021-03-25 22:01:29 -0500

    props, state, setState, event handling methods

--- src/App.js
+++ src/App.js
@@ -1,19 +1,40 @@
 import React, { Component } from "react";
 import "./App.css";
-import Person from "./Person/Person"
+import Person from "./Person/Person";
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: "Ce", age: 28 },
+      { name: "Stephanie", age: 23 },
+      { name: "Moises", age: 21 },
+    ],
+  };
+
+  switchNameHandler = () => {
+    //console.log("was clicked!");
+    // DONT DO THIS this.state.persons[0].name = "Celia Saumell"
+    this.setState({
+      persons: [
+        { name: "Celia Saumell", age: 28 },
+        { name: "Stephanie", age: 101 },
+        { name: "Moises", age: 21 },
+      ],
+    });
+  };
+
   render() {
     return (
       <div className="App">
         <h1>Hi!! I'm a React App</h1>
         <p>This is really working</p>
-        <Person />
-        <Person />
-        <Person />
-        
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>
+          My Hobbies: Reading
+        </Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
       </div>
-
     );
     // The above JSX code gets compiled into this code below
     // return React.createElement(
