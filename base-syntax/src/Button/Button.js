a8bcc004c1b556035267fe298962f9e09de2ecb3
Celia Saumell <56240891+celiasaumell@users.noreply.github.com>
2021-04-07 00:37:49 -0500

    installed webpack babel and syled-components

---
+++ base-syntax/src/Button/Button.js
@@ -0,0 +1,27 @@
+import React from "react";
+import styled, { css } from "styled-components";
+
+const Button = styled.button`
+  background: transparent;
+  border-radius: 3px;
+  border: 2px solid palevioletred;
+  color: palevioletred;
+  margin: 0 1em;
+  padding: 0.25em 1em;
+
+  ${(props) =>
+    props.primary &&
+    css`
+      background: palevioletred;
+      color: white;
+      &:hover{
+          color: purple;
+      }
+    `};
+`;
+
+const Container = styled.div`
+  text-align: center;
+`;
+
+export {Container, Button}
