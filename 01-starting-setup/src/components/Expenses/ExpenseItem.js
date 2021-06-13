2bb4dca8f629b7faefd2f7cbe6d148ad885e1212
mf11y <56240891+celiasaumell@users.noreply.github.com>
2021-06-13 18:03:12 -0500

    child to parent component communication

--- 01-starting-setup/src/components/Expenses/ExpenseItem.js
+++ 01-starting-setup/src/components/Expenses/ExpenseItem.js
@@ -1,17 +1,30 @@
-import ExpenseDate from './ExpenseDate'
-import Card from '../UI/Card'
-import './ExpenseItem.css'
+import React, { useState } from 'react'
 
-function ExpenseItem (props) {
+import ExpenseDate from "./ExpenseDate";
+import Card from "../UI/Card";
+import "./ExpenseItem.css";
+
+function ExpenseItem(props) {
+  //function clickHandler() {}
+  //React hook useState allows us to define values as states and changing values should reflect in component function being called again
+  //use directly in the React component function - not outside or in nested functions
+  //updated on a per component basis! (React will create different instances)
+  const [title, setTitle] = useState(props.title);
+
+  const clickHandler = () => {
+   setTitle("Updated!"); 
+  }
   return (
-    <Card className='expense-item'>
+    <Card className="expense-item">
       <ExpenseDate date={props.date} />
-      <div className='expense-item__desc'>
-        <h2>{props.title}</h2>
-        <div className='expense-item__price'>${props.amount}</div>
+      <div className="expense-item__desc">
+        <h2>{title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
+      
     </Card>
-  )
+  );
 }
 
-export default ExpenseItem
+export default ExpenseItem;
---
+++ 01-starting-setup/src/components/NewExpense/ExpenseForm.js
@@ -0,0 +1,76 @@
+import React, { useState } from "react";
+
+import "./ExpenseForm.css";
+
+function ExpenseForm(props) {
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
+
+  const titleChangeHandler = (event) => {
+    setEnteredTitle(event.target.value);
+  };
+  const amountChangeHandler = (event) => {
+    setEnteredAmount(event.target.value);
+  };
+  const dateChangeHandler = (event) => {
+    setEnteredDate(event.target.value);
+  };
+
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate),
+    };
+    props.onSaveExpenseData(expenseData);
+    setEnteredTitle("");
+    setEnteredAmount("");
+    setEnteredDate("");
+  };
+
+  const newExpenseControl = [
+    {
+      label: "Title",
+      value: enteredTitle,
+      handler: titleChangeHandler,
+      attributes: { type: "text" },
+    },
+    {
+      label: "Amount",
+      value: enteredAmount,
+      handler: amountChangeHandler,
+      attributes: { type: "number", min: "0.01", step: "0.01" },
+    },
+    {
+      label: "Date",
+      handler: dateChangeHandler,
+      value: enteredDate,
+      attributes: { type: "date", min: "2019-01-01", max: "2022-12-31" },
+    },
+  ];
+  return (
+    <form onSubmit={formSubmitHandler}>
+      <div className="new-expense__controls">
+        {newExpenseControl.map((control) => {
+          return (
+            <div className="new-expense__control">
+              <label>{control.label}</label>
+              <input
+                {...control.attributes}
+                value={control.value}
+                onChange={control.handler}
+              />
+            </div>
+          );
+        })}
+      </div>
+      <div className="new-expense__actions">
+        <button type="submit">Add Expense</button>
+      </div>
+    </form>
+  );
+}
+
+export default ExpenseForm;
