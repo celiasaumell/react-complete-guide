fb3b93612a8d94fb75565f522e20272d732c5eb7
mf11y <56240891+celiasaumell@users.noreply.github.com>
2021-06-21 21:17:55 -0500

    Working

---
+++ project1/src/Users/AddUser.js
@@ -0,0 +1,73 @@
+import React, { useState, useRef } from "react";
+
+import Card from "../UI/Card";
+import Button from "../UI/Button";
+import ErrorModal from "../UI/ErrorModal";
+import Wrapper from "../Helpers/Wrapper";
+
+import styles from "./AddUser.module.css";
+
+const AddUser = (props) => {
+  const nameInputRef = useRef();
+  const ageInputRef = useRef();
+
+  const [error, setError] = useState();
+
+  const addUserHandler = (e) => {
+    e.preventDefault();
+    const enteredName = nameInputRef.current.value;
+    const enteredUserAge = ageInputRef.current.value
+  
+    if (enteredName.trim().length === 0 || enteredUserAge.trim().length === 0) {
+      setError({
+        title: "Invalid Input",
+        message: "Please enter a valid name and age (non-empty values)",
+      });
+      return;
+    }
+    if (+enteredUserAge < 1) {
+      setError({
+        title: "Invalid Age",
+        message: "Please enter a valid age (greater than 0)",
+      });
+      return;
+    }
+    props.onAddUser(enteredName, enteredUserAge);
+
+  };
+
+  const errorHandler = () => {
+    setError(null);
+  };
+
+  return (
+    <Wrapper>
+      {error && (
+        <ErrorModal
+          title={error.title}
+          message={error.message}
+          onClick={errorHandler}
+        />
+      )}
+      <Card className={styles.input}>
+        <form onSubmit={addUserHandler}>
+          <label htmlFor="username">UserName</label>
+          <input
+            type="text"
+            id="username"
+            ref={nameInputRef}
+          />
+          <label htmlFor="age">Age (Years)</label>
+          <input
+            type="number"
+            id="age"
+            ref={ageInputRef}
+          />
+          <Button type="submit">Add User</Button>
+        </form>
+      </Card>
+    </Wrapper>
+  );
+};
+
+export default AddUser;
