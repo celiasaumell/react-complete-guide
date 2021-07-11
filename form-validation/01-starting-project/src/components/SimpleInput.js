3e835a03c49e9be92ef1a833dccba83ab07bbd7e
mf11y <56240891+celiasaumell@users.noreply.github.com>
2021-07-11 12:36:16 -0500

    form validation useRef and useState

---
+++ form-validation/01-starting-project/src/components/SimpleInput.js
@@ -0,0 +1,36 @@
+import { useState, useRef } from "react";
+
+const SimpleInput = (props) => {
+  const [enteredName, setEnteredName] = useState("");
+  const nameInputRef = useRef();
+  const nameInputChangeHandler = (event) => {
+    setEnteredName(event.target.value);
+  };
+
+  const formSubmissionHandler = (event) => {
+    event.preventDefault();
+
+    console.log(nameInputRef.current.value);
+    setEnteredName("")
+  };
+
+  return (
+    <form onSubmit={formSubmissionHandler}>
+      <div className="form-control">
+        <label htmlFor="name">Your Name</label>
+        <input
+          type="text"
+          id="name"
+          ref={nameInputRef}
+          onChange={nameInputChangeHandler}
+          value={enteredName}
+        />
+      </div>
+      <div className="form-actions">
+        <button>Submit</button>
+      </div>
+    </form>
+  );
+};
+
+export default SimpleInput;
